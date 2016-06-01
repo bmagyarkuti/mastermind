603b4989a44fdbfd12b44118f27c6db83371ca73
Barna Magyarkuti <bmagyarkuti@gmail.com>
2016-06-01 09:03:24 +0200

    added shuffling

--- public/js/gameModel.js
+++ public/js/gameModel.js
@@ -21,6 +21,26 @@ let createGameModel = function () {
         steps = 0;
     };
 
+    let shuffle = function(array) {
+        // from stackoverflow: http://stackoverflow.com/posts/2450976/revisions
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    };
+
     /**
      *  @param {Array<string>} guesses*/
     let evaluatePattern = function (guesses) {
@@ -44,6 +64,7 @@ let createGameModel = function () {
                 evals[i] = 'neither';
             }
         }
+        shuffle(evals);
         return evals;
     };
 
